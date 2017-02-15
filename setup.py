10ea5ee898ae4ff33b9ee56eb226e3c3a8133876
Peter Konneker <pkonneker@lexile.com>
2017-02-15 09:36:55 -0500

    Changing the version number of django accepted as well as fixint data files
    * the readme is now included as well
    * I mentioned some of these changes in the last commit, but didn't push them

--- setup.py
+++ setup.py
@@ -2,19 +2,17 @@ import os
 from setuptools import setup
 
 here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.md')).read()
 
 setup(
     name='django-static-version',
     version='0.1',
     packages=['static_version', 'static_version.templatetags'],
     description='Small lib to add a version numbers to urls',
-    long_description=README,
     author='Peter Konrad Konneker',
     author_email='pkonneker@lexile.com',
     url='https://github.com/MetaMetricsInc/django-static-version',
-    data_files = [("", ["LICENSE.txt"])],
+    data_files = [("", ["LICENSE.txt", "README.md"])],
     install_requires=[
-        'Django>=1.9,<1.10',
+        'Django>=1.8,<1.10',
     ]
 )
