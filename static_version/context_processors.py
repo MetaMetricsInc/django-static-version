7a1204196857cff8fb2ae2bc0ac683ea3282ad50
Peter Konneker <pkonneker@lexile.com>
2017-02-15 10:01:51 -0500

    Adding the boiler plate to the relevant source files

--- static_version/context_processors.py
+++ static_version/context_processors.py
@@ -1,3 +1,25 @@
+"""
+    DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+
+    Copyright (c) 2017 MetaMetrics Inc. All rights reserved.
+
+    The contents of this file are subject to the terms of either the BSD
+    or the Common Development and Distribution License("CDDL")
+    (collectively, the "License").  You may not use this file except in
+    compliance with the License.  You can obtain a copy of the License
+    from LICENSE.txt in the root of the source distribution.
+    See the License for the specific language governing permissions
+    and limitations under the License.
+
+    When distributing the software, include this License Header Notice in each
+    file and include the License file LICENSE.txt.
+
+    Modifications:
+    If applicable, add the following below the License Header, with the fields
+    enclosed by brackets [] replaced by your own identifying information:
+    "Portions Copyright [year] [name of copyright owner]"
+"""
+
 from django.conf import settings
 
 def static_urls(request):
