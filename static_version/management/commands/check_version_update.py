89138817dddafa30b1742aa49aacf7822777e13b
Peter Konneker <pkonneker@lexile.com>
2017-02-22 13:08:41 -0500

    First pass at error handling

--- static_version/management/commands/check_version_update.py
+++ static_version/management/commands/check_version_update.py
@@ -15,17 +15,30 @@ class Command(BaseCommand):
             # compare hashes and update version if neccessary
             # write out new version and hash
 
-        with open('VERSION', 'r') as version_file:
-            # We're only really interested in the first two lines of this file
-            old_version = next(version_file).strip()
-            old_hash = next(version_file).strip()
+        try:
+            with open('STATIC.VERSION', 'r') as version_file:
+                # We're only really interested in the first two lines of this file
+                old_version = next(version_file).strip()
+                old_hash = next(version_file).strip()
+        except IOError:
+            old_version = 0
+            old_hash = ""
+            print("No version file found, a new default file will be created at STATIC.VERSION")
+        except StopIteration:
+            old_hash = ""
 
         new_hash = dirhash(settings.STATIC_ROOT, 'md5')
         print("Directory '{}' hashed to value: {}".format(settings.STATIC_ROOT, new_hash))
 
         if old_hash != new_hash:
-            new_version = int(old_version) + 1
+            try:
+                new_version = int(old_version) + 1
+
+            except ValueError:
+                    print("Error: Improperly formatted version number. Only integers are currently allowed.")
+                    return
+
             print("Detected hash change, writing out the bumped version (v={}) and new hash.".format(new_version))
-            with open('VERSION', 'w') as version_file_out:
+            with open('STATIC.VERSION', 'w') as version_file_out:
                 version_file_out.write("{}\n".format(new_version))
                 version_file_out.write("{}\n".format(new_hash))
--- static_version/util/__init__.py
+++ static_version/util/__init__.py
@@ -1,7 +1,10 @@
 
 # A simple utility to parse the version out of the VERSION file's first line
 def parse_version():
-    with open('VERSION', 'r') as version_file:
-        old_version = next(version_file).strip()
+    try:
+        with open('STATIC.VERSION', 'r') as version_file:
+            old_version = next(version_file).strip()
+    except IOError:
+        return "0"
 
     return old_version
