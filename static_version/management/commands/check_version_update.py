903aec04d767590315485dc4f39dc7404df3168e
Peter Konneker <pkonneker@lexile.com>
2017-02-22 14:47:24 -0500

    Updating how the error handling flows

--- static_version/management/commands/check_version_update.py
+++ static_version/management/commands/check_version_update.py
@@ -14,29 +14,26 @@ class Command(BaseCommand):
             #   grab the static dir and hash that
             # compare hashes and update version if neccessary
             # write out new version and hash
+        old_version = 0
+        old_hash = ""
 
         try:
             with open('STATIC.VERSION', 'r') as version_file:
                 # We're only really interested in the first two lines of this file
-                old_version = next(version_file).strip()
+                old_version = int(next(version_file).strip())
                 old_hash = next(version_file).strip()
         except IOError:
-            old_version = 0
-            old_hash = ""
             print("No version file found, a new default file will be created at STATIC.VERSION")
         except StopIteration:
-            old_hash = ""
+            print("Hash missing, this will ensure a version bump, but is otherwise harmless.")
+        except ValueError:
+            print("Improperly formatted or missing version number. Assumed 0.")
 
         new_hash = dirhash(settings.STATIC_ROOT, 'md5')
         print("Directory '{}' hashed to value: {}".format(settings.STATIC_ROOT, new_hash))
 
         if old_hash != new_hash:
-            try:
-                new_version = int(old_version) + 1
-
-            except ValueError:
-                    print("Error: Improperly formatted version number. Only integers are currently allowed.")
-                    return
+            new_version = old_version + 1
 
             print("Detected hash change, writing out the bumped version (v={}) and new hash.".format(new_version))
             with open('STATIC.VERSION', 'w') as version_file_out:
